c8c4b6ab326dd0b13804c73dbd21e9559f6cd0e6
Nicolás Navarro <nicolas@myseocompany.co>
2025-02-01 12:30:44 -0500

    Ajsutar riuta

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\WAToolBoxController;
+use App\Http\Controllers\Api\WAToolBoxController;
 use App\Http\Controllers\APIController;
 
 Route::middleware('api')->group(
