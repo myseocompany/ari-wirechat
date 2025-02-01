3b98dd5dbdbc86c82055872082db9018d36e7d38
myseocompany <nicolas@myseocompany.co>
2025-02-01 09:20:25 -0500

    quitar linea con use comentado

--- app/Http/Controllers/Api/WAToolBoxController.php
+++ app/Http/Controllers/Api/WAToolBoxController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Customer;
 use App\Models\Message;
-//use App\Events\MessageReceived;
 use Illuminate\Support\Facades\Auth;
 
 use App\Services\MessageService;
