1e4de4761290988b6ee6840dd4451b506c22963d
Nicolás Navarro <nicolas@myseocompany.co>
2025-02-02 10:56:06 -0500

    saltar envio de imagen por url

--- app/Services/WAToolboxService.php
+++ app/Services/WAToolboxService.php
@@ -90,9 +90,9 @@ class WAToolboxService {
 
             // Decidir el tipo de mensaje
             if (isset($data['media_url'])) {
-                $payload['type'] = 'media';
+                $payload['type'] = 'text';
                 $payload['attachments'] = [config('app.url') . $data['media_url']];
-                $payload['content'] = [config('app.url') . $data['media_url']];
+                $payload['content'] = config('app.url') . $data['media_url'];
                 
             } else {
                 $payload['type'] = 'text';
