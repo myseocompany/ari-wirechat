6e95e5a4bc224a55c3bc396b706d409c7fbd5051
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-30 15:46:30 -0500

    Corregir else en side_show no cargar pagina nueva

--- resources/views/customers/side_show.blade.php
+++ resources/views/customers/side_show.blade.php
@@ -93,9 +93,9 @@
 
 
       <div>@if(isset($customer->phone))
-        <a  href="/customers/{{$customer->id}}/show" target="_empty">{{$customer->phone}}</a>/@endif
+        <a  href="/customers/{{$customer->id}}/show">{{$customer->phone}}</a>/@endif
         @if(isset($customer->phone2)) 
-        <a href="/customers/{{$customer->id}}/show"  target="_empty">{{$customer->phone2}}</a>@endif
+        <a href="/customers/{{$customer->id}}/show" >{{$customer->phone2}}</a>@endif
         / {{$customer->email}}
 
 
