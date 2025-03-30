3f0963f6cd86222f7cafdc75883e59eb0cbbe85a
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-30 16:37:49 -0500

    quitar el rojo del alert

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -26,13 +26,13 @@
 
 {{-- Alertas --}}
 @if (session('status'))
-<div class="alert alert-primary alert-dismissible" role="alert">
+<div class="alert-primary alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
   {!! html_entity_decode(session('status')) !!}
 </div>
 @endif
 @if (session('statusone'))
-<div class="alert alert-warning alert-dismissible" role="alert">
+<div class="alert-warning alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
   {!! html_entity_decode(session('statusone')) !!}
 </div>
