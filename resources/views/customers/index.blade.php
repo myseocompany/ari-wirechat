f11c835015f030ed6b14aa070b14e6faa842b2ec
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-30 16:28:47 -0500

    al actualizar, quitar el rojo

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -220,7 +220,7 @@
         </div>
   @endif
     @if (session('statusone'))
-          <div class="alert alert-warning alert-dismissible" role="alert">
+          <div class="alert alert-success alert-dismissible" role="alert">
           <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span></button>
             {!! html_entity_decode(session('statusone')) !!}
         </div>
